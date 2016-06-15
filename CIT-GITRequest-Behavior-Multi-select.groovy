395bfa67b0d6e0521b224018f011b9ae110d130e
cinguva <chander.inguva@gmail.com>
2016-06-15 11:41:01 -0500

    Update CIT-GITRequest-Behavior-Multi-select.groovy

--- CIT-GITRequest-Behavior-Multi-select.groovy
+++ CIT-GITRequest-Behavior-Multi-select.groovy
@@ -3,6 +3,13 @@ import com.atlassian.jira.component.ComponentAccessor
 def jpField = getFieldByName("JIRA Project")
 def urlField = getFieldByName("GitLab URL")
 def aurlField = getFieldByName("Additional URL's / Notes")
+
+def fvField = getFieldById("fixVersions")
+def sField = getFieldByName("Sprint")
+def tField = getFieldById("timetracking_originalestimate")
+def rField = getFieldById("timetracking_remainingestimate")
+def SPField = getFieldByName("Story Points")
+
 def crField = getFieldById(getFieldChanged())
 def selectedOption = crField.getValue() as String
 
@@ -14,6 +21,12 @@ if ((selectedOption.contains("GIT Repositories")))
     aurlField.setRequired(true)
     jpField.setHidden(false)
     jpField.setRequired(true)
+    
+    fvField.setHidden(true)
+    sField.setHidden(true)
+    tField.setHidden(true)
+    rField.setHidden(true)
+    SPField.setHidden(true)
   
 }
 else
@@ -24,4 +37,10 @@ else
     aurlField.setRequired(false)
     jpField.setHidden(true)
     jpField.setRequired(false)
+    
+    fvField.setHidden(true)
+    sField.setHidden(true)
+    tField.setHidden(true)
+    rField.setHidden(true)
+    SPField.setHidden(true)
 }
