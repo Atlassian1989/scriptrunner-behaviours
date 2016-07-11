2a04a6ace581d2be194bca41934e87730dce2e99
Chander Inguva <chander.inguva@gmail.com>
2016-07-11 14:07:29 -0500

    Initialiser for CIT JIRA Project

---
+++ CIT-Atlassian-Request-Initialiser.groovy
@@ -0,0 +1,38 @@
+def projectKey = getIssueContext().getProjectObject().getKey()
+def issueTypeName = getIssueContext().getIssueType().getName()
+
+def WSField = getFieldByName("Wiki Space Name")
+def SKField = getFieldByName("Space Key")
+def SAField = getFieldByName("Space Administrators")
+def CRField = getFieldByName("Change Request")
+def WField = getFieldByName("Watchers")
+def SField = getFieldByName("Sprint")
+//def LField = getFieldByName("label")
+
+if (issueTypeName == "Wiki Space Request")
+{
+    WSField.setHidden(false)
+    WSField.setRequired(true)
+    SKField.setHidden(false)
+    SKField.setRequired(true)
+    SAField.setHidden(false)
+    SAField.setRequired(true)
+    CRField.setHidden(true)
+    WField.setHidden(true)
+    SField.setHidden(false)
+    //LField.setHidden(true)
+}
+
+else
+ {
+    WSField.setHidden(true)
+    WSField.setRequired(false)
+    SKField.setHidden(true)
+    SKField.setRequired(false)
+    SAField.setHidden(true)
+    SAField.setRequired(false)
+    CRField.setHidden(false)
+    WField.setHidden(false)
+    SField.setHidden(false)
+   // LField.setHidden(true)
+}
