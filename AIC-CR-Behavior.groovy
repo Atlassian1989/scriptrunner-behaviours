29f54ba55068becd3372fe03087f593efb62174b
Chander Inguva <chander.inguva@gmail.com>
2016-07-19 10:53:04 -0500

    Added AIC-CR BEhavior

---
+++ AIC-CR-Behavior.groovy
@@ -0,0 +1,24 @@
+import com.atlassian.jira.component.ComponentAccessor
+def IKDField =  getFieldById(getFieldChanged())
+def KDNField = getFieldByName("Kit Document Number")
+
+def selectedOption = IKDField.getValue() as String
+
+if (selectedOption != "")
+{
+if (selectedOption == "Yes")
+{
+  KDNField.setHidden(false)
+  KDNField.setRequired(true)
+}
+else
+ {
+   KDNField.setHidden(true)
+   KDNField.setRequired(false)
+ }
+}
+else
+ {
+    KDNField.setHidden(true)
+   KDNField.setRequired(false) 
+ }
